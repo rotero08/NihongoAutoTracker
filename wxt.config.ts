936f69cee9b23a36a99fba2a0036b2b2f54bb7ac
rotero08 <roteroerazo2@gmail.com>
2026-05-07 06:47:57 -0500

    changed version

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   srcDir: 'src',
   manifest: {
     name: 'NihongoAutoTracker',
-    version: '3.1.2',
+    version: '3.1.3',
     permissions: ['storage', 'contextMenus', 'notifications', 'tabs'],
     host_permissions: [
       'https://nihongotracker.app/*',
